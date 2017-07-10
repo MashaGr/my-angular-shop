8bc7f90d76914b797e6ed14dfb8d02bc35692182
MariiaGrebennyk <marygrebennyk@gmail.com>
2017-07-10 12:01:37 +0300

    homework1_part1 (enum, array, onBuy, ngIf, ngFor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+enum CategoryType {
+  cat1 = 1 ,
+  cat2 = 2,
+  cat3 = 3
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  name: string;
+  description: string;
+  price: number;
+  category: CategoryType;
+  isAvailable: boolean;
+  ingredients: any[];
+  equivalents: any[];
+
+  ngOnInit() {
+    this.isAvailable = false;
+    this.ingredients = ['ingredient1', 'ingredient2'];
+    this.equivalents = ['equivalent3', 'equivalent4', 'equivalent5'];
+  }
+
+  onBuy() {
+    console.log('1');
+  }
+
 }
